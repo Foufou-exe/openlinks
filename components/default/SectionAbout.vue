1a3cfcc49d75da8e03e1da1c76727348f89f2512
Foufou-exe <thibaut.maurras34@gmail.com>
2024-01-17 02:12:18 -0500

    ✨ feat(NEW HOME): START
    .

--- components/default/SectionAbout.vue
+++ components/default/SectionAbout.vue
@@ -1,17 +1,28 @@
 <template>
-    <div>
-        <div>
-            <h1 class="font-bold text-4xl">Organise, Share and Protect Your Favourite Links with OpenLinks</h1>
-            <p class="font-thin">
-                <span class="font-semibold">Your Link Manager Designed for Confidentiality and Flexibility.</span> OpenLinks revolutionises the way you save and access your favourite links. Designed for modern professionals, students and everyone in between, our platform lets you store links to anything - from essential articles and video tutorials to collaborative project pages - and find them easily at any time, on any device.
-            </p>
-            <Button type="button" class="flex justify-center items-center">
-                <Icon name="circum:paperplane"  class="text-xl mr-1"/>
-                Get Started
-            </Button>
-        </div>
-        <div>
-            
+    <div class="w-full md:w-55/100 lg:pt-10 space-y-5">
+        <h1 class="font-bold text-4xl leading-none md:mt-0">Organise, Share and Protect Your Favourite Links with Openlinks <span class="animate-pulse">✨</span></h1>
+        <p class="text-lg font-normal">
+            Your Link Manager Designed for Confidentiality and Flexibility. <br>
+            OpenLinks revolutionises the way you save and access your favourite links. Designed for modern professionals, students and everyone in between, our platform lets you store links to anything - from essential articles and video tutorials to collaborative project pages - and find them easily at any time, on any device.
+        </p>
+        <Button type="button" class="flex items-center justify-center rounded-full ">
+            <Icon name="circum:paperplane"  class="text-xl mr-1"/>
+            Get Started
+        </Button>
+        <div class="flex items-end">
+            <a href="">
+                <Icon name="pajamas:github" class="text-3xl mr-2" />
+            </a>
         </div>
     </div>
+    <div class="hidden md:block md:w-full">
+        <client-only>
+            <Vue3Lottie :loop="false" animationLink="/json/index_image1.json" alt="OpenLinks"  class="ml-auto object-contain md:w-full lg:w-full"/>
+        </client-only>
+    </div>
+
 </template>
+
+<script lang="ts" setup>
+import { Vue3Lottie } from 'vue3-lottie'
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
     <!-- <MaintenceSite /> -->
     <div class="flex min-h-screen flex-col">
-        <section id="about" class="min-h-screen flex justify-center items-center">
+        <section id="about" class="min-h-screen mx-auto flex max-w-6xl items-center p-5 pt-16 pb-4 md:px-6 lg:px-8 lg:pt-12">
             <SectionAbout />
         </section>
         <section id="why" class="min-h-screen">
