cdb2f6e49c94601492af6afa58248b2abdcd662f
Foufou-exe <thibaut.maurras34@gmail.com>
2024-01-20 16:29:49 -0500

    🐞 fix(COMPONENTS): solving the grid
    The problem was that the size of each card was different

--- components/default/SectionWhy.vue
+++ components/default/SectionWhy.vue
@@ -1,10 +1,11 @@
 <template>
-    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5">
-        <div class="col-span-full mb-10">
+    <div class="grid grid-cols-1 gap-5">
+        <div class="mb-10">
             <div class="flex flex-col lg:flex-row justify-center items-center space-y-3 lg:space-y-0 lg:space-x-3 p-5 text-center lg:text-left">
                 <Icon name="circum:link" class="lg:text-7xl text-5xl" />
             </div>
-            <div class="flex flex-col lg:flex-row justify-center items-center space-y-3 lg:space-y-0 lg:space-x-3 p-5 text-center">
+            <div
+                class="flex flex-col lg:flex-row justify-center items-center space-y-3 lg:space-y-0 lg:space-x-3 text-center">
                 <div class="text-3xl lg:text-4xl font-bold">
                     <span>
                         Openlinks offers you a
@@ -17,83 +18,57 @@
             </div>
         </div>
 
-        <div class="p-5 border rounded-lg">
-            <div class="flex items-center space-x-3">
-                <Icon name="circum:lock" class="text-4xl" />
-                <h1 class="font-semibold text-lg">
-                    Uncompromising safety
-                </h1>
-            </div>
-            <div class="mt-3">
-                <p class="font-thin">
-                    Openlinks uses advanced encryption to protect your data. We use the latest type of encryption.
-                </p>
-            </div>
-        </div>
-        <div class="p-5 border rounded-lg">
-            <div class="flex items-center space-x-3">
-                <Icon name="circum:monitor" class="text-4xl" />
-                <h1 class="font-semibold text-lg">
-                    A Clean Interface
-                </h1>
-            </div>
-            <div class="mt-3">
-                <p class="font-thin">
-                    We've designed OpenLinks to be intuitively simple, eliminating the unnecessary so you can focus on what's important - your links.
-                </p>
-            </div>
-        </div>
-        <div class="p-5 border rounded-lg">
-            <div class="flex items-center space-x-3">
-                <Icon name="circum:microchip" class="text-4xl" />
-                <h1 class="font-semibold text-lg">
-                    Global Accessibility
-                </h1>
-            </div>
-            <div class="mt-3">
-                <p class="font-thin">
-                    Stay connected to your essential information with OpenLinks, wherever you are and on any device. Your links follow you whether you're on the move or at your desk.
-                </p>
-            </div>
-        </div>
-        <div class="p-5 border rounded-lg">
-            <div class="flex items-center space-x-3">
-                <Icon name="circum:dollar" class="text-4xl" />
-                <h1 class="font-semibold text-lg">
-                    Free to use
-                </h1>
-            </div>
-            <div class="mt-3">
-                <p class="font-thin">
-                    Openlinks is free to use. We offer a premium plan for those who want to support us.
-                </p>
-            </div>
-        </div>
-        <div class="p-5 border rounded-lg">
-            <div class="flex items-center space-x-3">
-                <Icon name="circum:bookmark" class="text-4xl" />
-                <h1 class="font-semibold text-lg">
-                    Quickly register your links
-                </h1>
-            </div>
-            <div class="mt-3">
-                <p class="font-thin">
-                    Openlinks lets you quickly register your links. You can register your links using our browser extension or by adding them manually.
-                </p>
-            </div>
-        </div>
-        <div class="p-5 border rounded-lg">
-            <div class="flex items-center space-x-3">
-                <Icon name="circum:chat-1" class="text-4xl" />
-                <h1 class="font-semibold text-lg">
-                    Support 24/7
-                </h1>
-            </div>
-            <div class="mt-3">
-                <p class="font-thin">
-                    We offer 24/7 support. If you have any questions, please contact us.
-                </p>
-            </div>
+        <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5 grid-rows-6">
+            <CardWhyVue 
+                v-for="(component, index) in componentsHelp"
+                :key="index"
+                :title="component.title"
+                :description="component.description"
+                :icons="component.icons"
+            />
         </div>
     </div>
 </template>
+
+<script lang="ts" setup>
+import CardWhyVue from '@/components/default/why/CardWhy.vue.vue';
+
+const componentsHelp: { title: string; description: string; icons: string; }[] = [
+  {
+    title: 'Uncompromising safety',
+    icons: 'circum:lock',
+    description:
+      'Openlinks uses advanced encryption to protect your data. We use the latest type of encryption.',
+  },
+  {
+    title: 'A Clean Interface',
+    icons: 'circum:monitor',
+    description:
+      'We ve designed OpenLinks to be intuitively simple, eliminating the unnecessary so you can focus on what s important - your links.',
+  },
+  {
+    title: 'Global Accessibility',
+    icons: 'circum:microchip',
+    description:
+      'Stay connected to your essential information with OpenLinks, wherever you are and on any device.  Your links follow you whether you re on the move or at your desk.',
+  },
+  {
+    title: 'Free to use',
+    icons: 'circum:dollar',
+    description:
+      'Openlinks is free to use. We offer a premium plan for those who want to support us.',
+  },
+  {
+    title: ' Quickly register your links',
+    icons: 'circum:bookmark',
+    description:
+      'Openlinks lets you quickly register your links. You can register your links using our browser extension or by adding them manually.',
+  },
+  {
+    title: 'Support 24/7',
+    icons: 'circum:chat-1',
+    description:
+      'We offer 24/7 support. If you have any questions, please contact us.',
+  },
+]
+</script>
---
+++ components/default/why/CardWhy.vue.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="p-5 border rounded-lg">
+        <div class="flex items-center space-x-3">
+            <Icon :name="icons" class="text-4xl" />
+            <span class="font-semibold text-lg">
+                {{ title }}
+            </span>
+        </div>
+        <div class="mt-3">
+            <p class="font-thin">
+                {{ description }}
+            </p>
+        </div>
+    </div>
+</template>
+
+<script lang="ts" setup>
+defineProps<{ title?: string; description?: string ,icons?: string }>()
+</script>
+
